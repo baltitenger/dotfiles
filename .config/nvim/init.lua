a5f4b15a5e1daa7fed43a82b89f335045698f01e
Baltazár Radics <baltazar.radics@gmail.com>
2021-07-03 18:06:51 +0200

    stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,84 +1,50 @@
 local datapath = vim.fn.stdpath('data')
 
 if vim.env.TERM ~= 'linux' then
-	vim.o.termguicolors = true
+	vim.opt.termguicolors = true
 end
 
-vim.cmd[[
-set ignorecase smartcase
-set scrolloff=3 sidescrolloff=6 lazyredraw
-set linebreak breakindent
-set list listchars=tab:\ \ ,extends:>,precedes:<,nbsp:~
-set background=dark
-set laststatus=1 helpheight=0
-set title titlestring=nvim\ %f\ %m
-set mouse=nv
-set undofile formatoptions=croq1jp completeopt=menuone,noinsert,noselect
-set tabstop=2 shiftwidth=0 smartindent
-set foldlevelstart=99 foldmethod=syntax
-set diffopt=filler,iwhite,closeoff,internal,indent-heuristic
-set wildmode=longest:full,full
-set virtualedit=block
-set inccommand=nosplit
-set clipboard=unnamed
-]]
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 6
+vim.opt.lazyredraw = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '  ', extends = '>', precedes = '<', nbsp = '~' }
+vim.opt.background = 'dark'
+vim.opt.laststatus = 1
+vim.opt.helpheight = 0
+vim.opt.title = true
+vim.opt.titlestring = 'nvim %f %m'
+vim.opt.mouse = 'nv'
+vim.opt.undofile = true
+vim.opt.formatoptions = 'croq1jp'
+vim.opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0
+vim.opt.smartindent = true
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = 'syntax'
+vim.opt.diffopt = { 'filler', 'iwhite', 'closeoff', 'internal', 'indent-heuristic' }
+vim.opt.wildmode = { 'longest:full', 'full' }
+vim.opt.virtualedit = 'block'
+vim.opt.inccommand = 'nosplit'
+vim.opt.clipboard = 'unnamed'
 
-vim.cmd[[au FileType tex setlocal spell]]
-vim.cmd[[au FileType markdown setlocal spell tw=80]]
-vim.cmd[[au FileType markdown compiler markdown]]
---vim.cmd[[au FileType python setlocal foldmethod=indent]]
-vim.cmd[[au FileType cs compiler dotnet]]
-vim.cmd[[au BufWritePre /tmp/* setlocal noundofile]]
-vim.cmd[[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line('$') && &ft !~# 'commit' | exe 'normal! g`"' | endif]]
+vim.cmd[==[
+au FileType tex setlocal spell
+au FileType markdown setlocal spell tw=80
+au FileType markdown compiler markdown
+"au FileType python setlocal foldmethod=indent
+au FileType cs compiler dotnet
+au BufWritePre /tmp/* setlocal noundofile
+au BufReadPost * if line("'\"") > 1 && line("'\"") <= line('$') && &ft !~# 'commit' | exe 'normal! g`"' | endif
 
-vim.g.netrw_banner = 0
-vim.g.python_recommended_style = 0
-vim.g.tex_comment_nospell = 1
-vim.g.tex_fold_enabled = 1
-vim.g.man_hardwrap = 0
+com! DiffOrig vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis
+com! -bang W lua SudoWrite('<bang>' == '!')
 
-vim.cmd[[command! DiffOrig vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis]]
---vim.cmd[[command! -bang W if empty('<bang>') | echoerr 'Add ! to use sudo' | else | w! /tmp/sudonvim | bel 2new +startinsert term://</tmp/sudonvim\ sudo\ tee\ >/dev/null\ % | endif]]
-
-vim.api.nvim_set_keymap('n', '<Leader>/', '<Cmd>noh<CR>', {})
-vim.api.nvim_set_keymap('n', 'Y', 'y$', {})
-vim.api.nvim_set_keymap('n', 'gb', '<Cmd>lua GitBlame()<CR>', {})
-vim.api.nvim_set_keymap('v', '<Leader>w', '<Cmd>lua VisualWc()<CR>', {})
-vim.api.nvim_set_keymap('v', '<LeftRelease>', 'y', {})
-vim.api.nvim_set_keymap('i', '<C-BS>', '<C-W>', {})
-
-function GitBlame()
-	if vim.w.gitblame_open then
-		vim.cmd('close '..vim.w.gitblame_open)
-		vim.w.gitblame_open = nil
-	else
-		vim.cmd'vnew'
-		vim.bo.buftype = 'nofile'
-		vim.cmd('r!git blame '..vim.fn.shellescape(vim.fn.expand('#'))..[[|sed 's/(//;s/\s\+[0-9]\+)\s.*$//']])
-		vim.cmd'0d_'
-		vim.bo.filetype = 'gitblame'
-		vim.cmd('vert res '..vim.fn.col('$')-1)
-		vim.wo.scrollbind = true
-		vim.cmd'winc p'
-		vim.wo.scrollbind = true
-		vim.cmd'sync'
-		vim.w.gitblame_open = vim.fn.winnr('#')
-	end
-end
-
-function VisualWc()
-	local wc = vim.fn.wordcount()
-	local lines = 1-vim.fn.line("'<")+vim.fn.line("'>")
-	print(lines, wc.visual_words, wc.visual_chars, '\n')
-end
-
-
-if vim.fn.exists('g:vscode') == 1 then
-	return
-end
-
-
-vim.cmd[[
 hi Error        guibg=DarkRed
 hi ErrorMsg     guibg=DarkRed
 hi Folded       guibg=NONE guifg=Cyan
@@ -93,7 +59,66 @@ hi debugPC      guibg=DarkBlue
 hi NonText      guifg=DarkCyan
 hi manUnderline guifg=Green
 hi manBold      gui=bold
-]]
+]==]
+
+vim.g.netrw_banner = 0
+vim.g.python_recommended_style = 0
+vim.g.tex_comment_nospell = 1
+vim.g.tex_fold_enabled = 1
+vim.g.man_hardwrap = 0
+
+vim.api.nvim_set_keymap('n', '<Leader>/', '<Cmd>noh<CR>', {})
+vim.api.nvim_set_keymap('n', 'Y', 'y$', {})
+vim.api.nvim_set_keymap('n', 'gb', '<Cmd>lua GitBlame()<CR>', {})
+vim.api.nvim_set_keymap('v', '<Leader>w', '<Cmd>lua VisualWc()<CR>', {})
+vim.api.nvim_set_keymap('v', '<LeftRelease>', 'y', {})
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', {})
+
+function SudoWrite(sure)
+	if not sure then
+		print 'Add ! to use sudo.'
+	else
+		vim.cmd([[
+			w! /tmp/sudonvim
+			bel 2new
+			startinsert
+			te </tmp/sudonvim sudo tee >/dev/null ]]..vim.fn.shellescape(vim.fn.expand('%'))
+		)
+	end
+end
+
+function GitBlame()
+	if vim.w.gitblame_open then
+		vim.cmd('close '..vim.w.gitblame_open)
+		vim.w.gitblame_open = nil
+	else
+		local winnr = vim.fn.winnr()
+		local width = vim.fn.winwidth(winnr)
+		vim.cmd 'vnew'
+		vim.w.gitblame_open = winnr
+		vim.opt_local.buftype = 'nofile'
+		vim.cmd('r!git blame '..vim.fn.shellescape(vim.fn.expand('#'))..[[|sed 's/(//;s/\s\+[0-9]\+)\s.*$//']])
+		vim.cmd '0d_'
+		vim.opt_local.filetype = 'gitblame'
+		vim.opt_local.wrap = false
+		vim.cmd('vert res '..math.min(vim.fn.col('$') - 1, width / 3))
+		vim.opt_local.scrollbind = true
+		vim.cmd 'winc p'
+		vim.opt_local.scrollbind = true
+		vim.cmd 'sync'
+		vim.w.gitblame_open = winnr
+	end
+end
+
+function VisualWc()
+	local wc = vim.fn.wordcount()
+	local lines = 1-vim.fn.line("'<")+vim.fn.line("'>")
+	print(lines, wc.visual_words, wc.visual_chars, '\n')
+end
+
+
+if vim.fn.exists('g:vscode') == 1 then return end
+
 
 local function executable(cmd)
 	return vim.fn.executable(cmd) == 1
@@ -143,7 +168,7 @@ Plug 'bogado/file-line'
 
 -- Plug 'sakhnik/nvim-gdb'
 
-vim.lsp.set_log_level('trace');
+-- vim.lsp.set_log_level('trace');
 
 Plug 'neovim/nvim-lspconfig'
 table.insert(after, function()
@@ -185,104 +210,54 @@ table.insert(after, function()
 		if client.resolved_capabilities.document_range_formatting then
 			buf_set_keymap('v', ' f', ':lua vim.lsp.buf.range_formatting()<CR>')
 		end
-
-		-- Set autocommands conditional on server_capabilities
-		if client.resolved_capabilities.document_highlight then
-			--lspconfig.util.nvim_multiline_command [[
-			--	augroup lsp_document_highlight
-			--		autocmd!
-			--		autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-			--		autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-			--	augroup END
-			--]]
-		end
 	end
 
-	if false and executable('ccls') then
-		lspconfig.ccls.setup{
-			capabilities = capabilities, on_attach = on_attach,
-		}
-	elseif executable('clangd') then
-		lspconfig.clangd.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			cmd = { 'clangd', '--background-index', '--clang-tidy', '--completion-style=detailed', '--header-insertion=iwyu' },
-		}
-	end
-	if executable('vscode-html-languageserver') then
-		lspconfig.html.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			cmd = { 'vscode-html-languageserver', '--stdio' },
-		}
-	end
-	if executable('vscode-css-languageserver') then
-		lspconfig.cssls.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			cmd = { 'vscode-css-languageserver', '--stdio' },
-		}
-	end
-	if executable('vscode-json-languageserver') then
-		lspconfig.jsonls.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			cmd = { 'vscode-json-languageserver', '--stdio' },
-		}
-	end
-	if executable('typescript-language-server') then
-		lspconfig.tsserver.setup{
-			capabilities = capabilities, on_attach = on_attach,
-		}
-	end
-	if executable('pyright-langserver') then
-		lspconfig.pyright.setup{
-			capabilities = capabilities, on_attach = on_attach,
-		}
-	elseif executable('pyls') then
-		lspconfig.pyls.setup{
-			capabilities = capabilities, on_attach = on_attach,
-		}
-	end
-	if executable('texlab') then
-		lspconfig.texlab.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			settings = {
-				latex = {
-					build = {
-						executable = 'latexmk',
-						args = { '-pdf', '-interaction=nonstopmode', '-synctex=1', '-lualatex', '-outdir=build', '%f' },
-						outputDirectory = 'build',
-						onSave = true,
-					},
-					forwardSearch = {
-						executable = 'llpp.inotify',
-						args = { '%p' },
-					},
+	lspconfig.clangd.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		cmd = { 'clangd', '--background-index', '--clang-tidy', '--completion-style=detailed', '--header-insertion=iwyu' },
+	}
+	lspconfig.html.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		cmd = { 'vscode-html-languageserver', '--stdio' },
+	}
+	lspconfig.cssls.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		cmd = { 'vscode-css-languageserver', '--stdio' },
+	}
+	lspconfig.jsonls.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		cmd = { 'vscode-json-languageserver', '--stdio' },
+	}
+	lspconfig.tsserver.setup{
+		capabilities = capabilities, on_attach = on_attach,
+	}
+	lspconfig.pyright.setup{
+		capabilities = capabilities, on_attach = on_attach,
+	}
+	lspconfig.texlab.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		settings = {
+			latex = {
+				build = {
+					executable = 'latexmk',
+					args = { '-pdf', '-interaction=nonstopmode', '-synctex=1', '-lualatex', '-outdir=build', '%f' },
+					outputDirectory = 'build',
+					onSave = true,
+				},
+				forwardSearch = {
+					executable = 'llpp.inotify',
+					args = { '%p' },
 				},
 			},
-		}
-	end
-	if executable('omnisharp') then
-		lspconfig.omnisharp.setup{
-			capabilities = capabilities, on_attach = on_attach,
-			cmd = { 'omnisharp', '-lsp', '-hpid', tostring(vim.fn.getpid()) },
-		}
-	end
+		},
+	}
+	lspconfig.omnisharp.setup{
+		capabilities = capabilities, on_attach = on_attach,
+		cmd = { 'omnisharp', '-lsp', '-hpid', tostring(vim.fn.getpid()) },
+	}
 end)
 
---Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
---table.insert(after, function()
---	require'nvim-treesitter.configs'.setup{
---		ensure_installed = { 'c', 'cpp', 'c_sharp', 'javascript', 'lua', 'python', 'typescript' },
---		highlight = {
---			enable = true,
---		},
---		--indent = {
---		--	enable = true
---		--}
---	}
---	vim.o.foldmethod = 'expr'
---	vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
---end)
-
-Plug 'nvim-lua/completion-nvim'
+lug 'nvim-lua/completion-nvim'
 vim.g.completion_sorting = 'none'
 vim.g.completion_enable_snippet = 'vim-vsnip'
 vim.g.completion_matching_smart_case = 1
@@ -312,23 +287,6 @@ smap <expr> <S-Tab> vsnip#available(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-T
 
 Plug 'qpkorr/vim-renamer'
 vim.g.RenamerSupportColonWToRename = 1
-
---Plug 'mfussenegger/nvim-dap'
---table.insert(after, function()
---	local dap = require('dap')
---	dap.adapters.cpp = {
---		name = 'lldb',
---		type = 'executable',
---		command = 'lldb-vscode',
---		env = {
---			LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = 'YES',
---		},
---		attach = {
---			pidProperty = 'pid',
---			pidSelect = 'ask',
---		},
---	}
---end)
 
 Plug 'peterhoeg/vim-qml'
 
---
+++ .local/share/nvim/site/syntax/gitblame.vim
@@ -0,0 +1,17 @@
+
+" quit when a syntax file was already loaded
+if exists('b:current_syntax') && b:current_syntax == 'gitblame'
+  finish
+endif
+
+syn match gitblamePadding '^^*' contained
+syn match gitblameHash    '^^*\x*' contains=gitblamePadding
+syn match gitblameDate    '\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d +\d\d\d\d$'
+syn match gitblameNotcomm '^00000000 Not Committed Yet'
+
+hi def link gitblamePadding Special
+hi def link gitblameHash    Label
+hi def link gitblameDate    Constant
+hi def link gitblameNotcomm Comment
+
+let b:current_syntax = 'gitblame'
